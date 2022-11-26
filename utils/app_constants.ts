aa8ed4c9f7a36a163fb3e1b5ad7483fbc0fd28b9
Jay Cabsasag <jay@metromart.com>
2022-11-26 21:35:40 +0800

    New commit

--- utils/app_constants.ts
+++ utils/app_constants.ts
@@ -8,7 +8,7 @@ export const COLORS = {
 
 export const IMAGES = {
     NO_IMAGE_AVAILABLE: 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930',
-    LOGIN_IMG_BACKGROUND_IMG: 'https://tcu.taguig.gov.ph/wp-content/uploads/2020/09/bg.png',
+    LOGIN_IMG_BACKGROUND_IMG: 'https://firebasestorage.googleapis.com/v0/b/tcumobilelibrary.appspot.com/o/images%2Fbg.png?alt=media&token=98231f44-efdb-44db-bc12-eb3ab3bc346d',
     PROFILE_IMAGE_NOT_AVAILABLE: 'https://pbs.twimg.com/profile_images/573692360263004161/gOvizBEP_400x400.jpeg'
 }
 
